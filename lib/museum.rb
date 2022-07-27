372ffb524c41fae00d4e330d90c2bf3fb7dcc8ff
Astrid Hecht <silentaviary@gmail.com>
2022-07-27 10:18:53 -0600

    feat: write museum class & methods

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,2 +1,19 @@
+require_relative './exhibit'
+require_relative './patron'
+
 class Museum
+  attr_reader :name, :exhibits
+
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit if exhibit.is_a? Exhibit
+  end
+
+  def reccomend_exhibits(patron)
+    @exhibits.find_all { |exhibit| patron.interests.include?(exhibit.name)}
+  end
 end
