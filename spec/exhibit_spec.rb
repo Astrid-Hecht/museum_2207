2e58a0c59ef86f055ad950d878e559cc4ade1e8d
Astrid Hecht <silentaviary@gmail.com>
2022-07-27 09:45:41 -0600

    test: write exhibit tests

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/exhibit.rb'
+
+RSpec.describe do 
+  before :each do 
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  it 'exists' do 
+    expect(@exhibit).to be_a(Exhibit)
+  end
+
+  it 'has a name' do
+    expect(@exhibit.name).to eq("Gems and Minerals")
+  end
+  
+  it 'has a cost' do
+    expect(@exhibit.cost).to eq 0
+  end
+end
